e193145793c326b1d6053462b30a701c595de3d1
doneit-cpu <studtydogone@gmail.com>
2025-01-03 08:25:19 +0530

    near to end , deliverydate 14:42

--- data/cart.js
+++ data/cart.js
@@ -1,14 +1,14 @@
-
-
-export let  cart=JSON.parse(localStorage.getItem('cart')) // parse convert string to disired data type 
+export let cart=JSON.parse(localStorage.getItem('cart')) // parse convert string to disired data type 
 
 if(!cart){
-[{
+  cart=[{
   productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-  quantity: 2,
+  quantity: 2, //shock
+  deliveryOptionsId: '1'
 },{
   productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1,
+  quantity: 1,  //basketball
+  deliveryOptionsId: '2'
 }];
 }
 function saveToStorage(){
@@ -17,7 +17,7 @@ function saveToStorage(){
 
 export function addtocart(productId){  
   let matchingItem ;
-      // console.log(cart,'lol'); // why this showed me anyhting here , when it is empty ,
+      console.log(cart,'lol'); // why this showed me anyhting here , when it is empty ,
       cart.forEach((item) => {  
         // console.log(item.productId);
         if (productId === item.productId) {
@@ -26,11 +26,12 @@ export function addtocart(productId){
       })
       console.log(matchingItem);  // why undifined 
       if (matchingItem) {
-        matchingItem.quantity += 1;
+        matchingItem.quantity += 1; // add 
       } else {
         cart.push({
           productId: productId,
           quantity: 1,
+          deliveryOptionsId:'1'
         });
       }
 
@@ -54,4 +55,20 @@ export function removeFromCart(productId){
   saveToStorage();
 };
 
+
+export function updatedeliveryOption(productId,deliveryOptionsId){
+
+  let matchingItem ;
+      
+      cart.forEach((item) => {  
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      })
+
+      matchingItem.deliveryOptionsId= deliveryOptionsId;
+      // console.log(deliveryOptionsId);
+      saveToStorage();
+
+}
 
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,25 +1,52 @@
-import {cart,removeFromCart} from '../data/cart.js';
+import {cart,removeFromCart, updatedeliveryOption} from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
+import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'  
+import { deliveryOptions} from '../data/deliveryOptions.js'
+
+// hell0();
+const today=dayjs();
+const deliveryDate =today.add(0,'days');
+const dateString = deliveryDate.format('dddd, MMMM D');
 let cartSummaryHTML = '';
-
+// console.log(cart)
 cart.forEach((cartItem) =>{
-  const productId=cartItem.productId;
+      const productId=cartItem.productId;
 
-  let matchingProduct;
+      let matchingProduct;
 
-  products.forEach((product)=>{
-    if (product.id === productId){
-      matchingProduct = product;
-    }
+      products.forEach((product)=>{
+        if (product.id === productId){
+          matchingProduct = product;
+        } 
+      });
+ //esm version ,ecmascript is also name for jabascript
+
+   
+      const deliveryOptionId = cartItem.deliveryOptionsId;
+      // console.log(deliveryOptionId)
+
+      let deliveryOption ;
+
+      deliveryOptions.forEach((option)=>{
+        if (option.id===deliveryOptionId){
+          deliveryOption=option;
+        }
+      });
+      // console.log(deliveryOption)
+      const today=dayjs();
+      const deliveryDate=today.add(
+        deliveryOption.deliveryDays,
+        'days'
+      );
+  
+      const dateString = deliveryDate.format('dddd, MMMM D')
+      
 
 
-
-  });
- 
   cartSummaryHTML += `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
-<div class="delivery-date">
-  Delivery date: Tuesday, June 21
+<div class="delivery-date ">
+  Delivery date: ${dateString}
 </div>
 
 <div class="cart-item-details-grid">
@@ -42,7 +69,7 @@ cart.forEach((cartItem) =>{
         Update
       </span>
       <span class="delete-quantity-link link-primary
-      js-delete-link" data-product-id="${matchingProduct.id}">
+      js-delete-link" data-product-id="${matchingProduct.id}"> 
         Delete
       </span>
     </div>
@@ -52,62 +79,97 @@ cart.forEach((cartItem) =>{
     <div class="delivery-options-title">
       Choose a delivery option:
     </div>
-    <div class="delivery-option">
-      <input type="radio" checked
-        class="delivery-option-input"
-        name="delivery-option-${matchingProduct.id}">
-      <div>
-        <div class="delivery-option-date">
-          Tuesday, June 21
-        </div>
-        <div class="delivery-option-price">
-          FREE Shipping
-        </div>
-      </div>
-    </div>
-    <div class="delivery-option">
-      <input type="radio"
-        class="delivery-option-input"
-        name="delivery-option-${matchingProduct.id}">
-      <div>
-        <div class="delivery-option-date">
-          Wednesday, June 15
-        </div>
-        <div class="delivery-option-price">
-          $4.99 - Shipping
-        </div>
-      </div>
-    </div>
-    <div class="delivery-option">
-      <input type="radio"
-        class="delivery-option-input"
-        name="delivery-option-${matchingProduct.id}">
-      <div>
-        <div class="delivery-option-date">
-          Monday, June 13
-        </div>
-        <div class="delivery-option-price">
-          $9.99 - Shipping
-        </div>
-      </div>
-    </div>
+    ${deliveryOptionsHTML(matchingProduct,cartItem)}
+    
   </div>
 </div>
-</div>`;
+</div>`; 
 
 })
 
+function deliveryOptionsHTML(matchingProduct,cartItem){
+
+  let html='';
+
+  deliveryOptions.forEach((deliveryOption)=>{  // this give us three type of shipping the option
+    const today=dayjs();
+    const deliveryDate=today.add(
+      deliveryOption.deliveryDays,
+      'days'
+
+    );
+
+    const dateString = deliveryDate.format('dddd, MMMM D')
+    ;
+    const priceString = deliveryOption.priceCents 
+    === 0
+      ? 'FREE'
+      : `$${formatCurrency(deliveryOption.priceCents)} -`;
+    // console.log(deliveryOption.id);
+    // console.log(cartItem);
+    // console.log(matchingProduct.id,'id')
+    const isChecked = deliveryOption.id ===
+    cartItem.deliveryOptionsId; // 
+    html +=
+    `<div class="delivery-option js-delivery-option"
+      data-product-id="${matchingProduct.id}"
+      data-deliveryoptions-id="${deliveryOption.id}">
+      <input type="radio"
+        ${isChecked ? 'checked' : ''} 
+        
+        class="delivery-option-input"
+        name="delivery-option-${matchingProduct.id}">
+      <div>
+        <div class="delivery-option-date">
+         ${dateString}
+        </div>
+        <div class="delivery-option-price">
+          ${priceString} Shipping
+        </div>
+      </div>
+    </div>`
+  });
+
+  return html;
+  
+}
+
 // 13:48
 document.querySelector('.js-order-summary').innerHTML=cartSummaryHTML;
-console.log(cartSummaryHTML);
+// console.log(cartSummaryHTML);
 
 document.querySelectorAll('.js-delete-link').forEach((link)=>{
   link.addEventListener('click',()=>{
     const productId =link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
 
     const container=document.querySelector(`.js-cart-item-container-${productId}`);
-      container.remove();  // this will remoeve whole html fromdom , shoert cut for remove item from webpage
+    console.log(container);
+      container.remove();  // this will remoeve whole html from (dom), shoert cut for remove item from webpage // like an dyanmic remove the item from display 
   });
 });
+
+//14:31
+
+document.querySelectorAll('.js-delivery-option')
+  .forEach((element)=>{
+    
+    element.addEventListener('click',()=>{
+      const {productId,deliveryoptionsId}=element.dataset;
+      // console.log(productId,deliveryoptionsId)
+      updatedeliveryOption(productId,deliveryoptionsId);
+      
+    })
+  })
+
+
+// function dilverydatechange(){
+
+//   document.querySelectorAll('.js-deliverdate-change').forEach((element)=>{  //it give all the things from html which evre has class name  
+//     const  deliverydateId=element.dataset.deliverydateId;  // this gives indiviual id for delivrydate div , like for shock and baksetball diff 
+   
+        
+//     });
+
+// }
+// 14.52
